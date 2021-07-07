cba6058a7b1212a2ac85cbbde9b7e07c8fb72d5b
Shirley Berchel <berchel.shirley@gmail.com>
2021-07-07 08:25:06 +0200

    send form to view twig

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use App\Form\PersonneType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PersonneController extends AbstractController
@@ -13,6 +16,24 @@ class PersonneController extends AbstractController
     {
         return $this->render('personne/index.html.twig', [
             'controller_name' => 'PersonneController',
+        ]);
+    }
+
+    #[Route('/personne/add', name: 'personne_add', methods:['GET','POST'])]
+    public function add(Request $request): Response
+    {
+        $personne = new Personne();
+        $form = $this->createForm(PersonneType::class, $personne);
+        $form->handleRequest($request);
+        if ( $form->isSubmitted() && $form->isValid() ) {
+            $personne = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($personne);
+            $em->flush();
+            return $this->redirectToRoute("personne");
+        }
+        return $this->render('personne/add.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
