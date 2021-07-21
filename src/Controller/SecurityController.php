e0b60923eb7efac17251866df0a480aa3b05791b
Shirley Berchel <berchel.shirley@gmail.com>
2021-07-21 13:51:24 +0200

    register new user form done

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -33,5 +43,31 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(Request $request)
+    {
+        //sharemycode/gxv
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ( $form->isSubmitted() && $form->isValid() ) {
+            $user = $form->getData();
+            //encoder ou hasher le mdp
+            //récupere le mdp avec la methode getPassword()
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $user->getPassword()));
+            //initialiser dateInscription
+            $user->setDateInscription(new \DateTime());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute("app_login");
+        }
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -20,10 +21,16 @@ class UserFixtures extends Fixture
         $user->setEmail('wick@wick.us');
         $user->setRoles (['ROLE_ADMIN']);
         $user->setPassword($this->passwordEncoder->encodePassword($user, 'wick'));
+        $user->setNom('Wick');
+        $user->setPrenom('John');
+        $user->setDateInscription(new \DateTime());
         $manager->persist($user);
         $user2 = new User();
         $user2->setEmail('john@john.us');
         $user2->setPassword($this ->passwordEncoder->encodePassword($user2, 'john'));
+        $user2->setNom('Doe');
+        $user2->setPrenom('John');
+        $user2->setDateInscription(new \DateTime("1996-09-19"));
         $manager->persist($user2);
         $manager->flush();
     }
